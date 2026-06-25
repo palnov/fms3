5948b44a729c6e3cf4f9bc312b99bb4f9d19abf4
Alex <palnov.aa@gmail.com>
2026-06-25 15:54:02 +0400

    Add Yandex Metrika tracking to the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -47,6 +48,24 @@ export default function RootLayout({
   return (
     <html lang="ru" className={`${golos.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+            ym(47198382, 'init', {webvisor:true, clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="https://mc.yandex.ru/watch/47198382" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
         <a className="skip-link" href="#main-content">Перейти к содержанию</a>
         <Header />
         <main id="main-content" className="flex-grow flex flex-col">{children}</main>
