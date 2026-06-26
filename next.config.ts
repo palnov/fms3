ca72e451beb1875c99ecbce68f65aa2c65fbb9e7
Alex <palnov.aa@gmail.com>
2026-06-26 12:24:39 +0400

    Enable GFM support in MDX

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
 
 const nextConfig: NextConfig = {
   output: "standalone",
@@ -38,7 +39,9 @@ const nextConfig: NextConfig = {
 };
 
 const withMDX = createMDX({
-  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm],
+  },
 });
 
 export default withMDX(nextConfig);
